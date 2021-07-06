9c2ec97e5c6af0ad745ace68e1685b7a64b63fdc
Benzer Bett <benzerbett@gmail.com>
2021-07-06 18:19:33 +0300

    abstracting sampel data to next api

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'display': ['Inter', 'sans-serif'],
+      'sans': ['Inter', 'sans-serif'],
+      'mono': ['monospace'],
+      'hand': ['Yomogi'],
+      'condensed': ['Oswald']
+    },
     extend: {
-      fontFamily: {
-        'display': ['Inter', 'sans-serif'],
-        'sans': ['Inter', 'sans-serif'],
-        'mono': ['monospace'],
-        'hand': ['Yomogi'],
-        'condensed': ['Oswald']
-      },
     },
   },
   variants: {
